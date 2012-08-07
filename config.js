4f0d64d574dffb773e28d5b524d6866d47416807
nobuhito <nobuhito.sato@gmail.com>
2012-08-07 15:43:55 +0900

    共通変数を別ファイルへ移動

---
+++ config.js
@@ -0,0 +1,27 @@
+var config = [
+    {
+        'name':  'both',
+        'str':   '♥',
+        'color': "#ffb6c1",
+        'type':  'text'},
+    {
+        'name':  'love',
+        'str':   '◀',
+        'color': "#778899",
+        'type':  'text'},
+    {
+        'name':  'orz',
+        'str':    '▶',
+        'color':  "#778899",
+        'type':   'text'},
+    {
+        'name':  'me',
+        'str':   '★',
+        'color': "#F9F06F",
+        'type':  'text'},
+    {
+        'name':  'fav',
+        'str':   '●',
+        'color': "#c90926",
+        'type':  'textarea'}
+];
--- options.js
+++ options.js
@@ -1,31 +1,3 @@
-var config = [
-    {
-        'name':  'both',
-        'str':   '♥',
-        'color': "#ffb6c1",
-        'type':  'text'},
-    {
-        'name':  'love',
-        'str':   '◀',
-        'color': "#778899",
-        'type':  'text'},
-    {
-        'name':  'orz',
-        'str':    '▶',
-        'color':  "#778899",
-        'type':   'text'},
-    {
-        'name':  'me',
-        'str':   '★',
-        'color': "#F9F06F",
-        'type':  'text'},
-    {
-        'name':  'fav',
-        'str':   '●',
-        'color': "#c90926",
-        'type':  'textarea'}
-];
-
 function $(id) {
     return document.getElementById(id);
 }
